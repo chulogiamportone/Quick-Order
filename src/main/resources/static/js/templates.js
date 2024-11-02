8a9efab41dfb74609ac41a6528f3903f774d2a3e
ChuchuGiampo <chulogiamportone@gmail.com>
2024-11-02 20:02:11 -0300

    cards en templates Bv1.1

---
+++ src/main/resources/static/js/templates.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	cargarPedidos();
+});
+
+
+
+async function cargarPedidos() {
+ 
+	 const response = await fetch('pedidos', {
+     method: 'GET',
+     headers: { 'Accept': 'application/json','Content-Type': 'application/json' },
+     
+    });
+    const pedidos = await response.json();
+    
+    let listadoHtml='';
+    let pasada=0;
+    for(let pedido of pedidos){
+		pasada++
+		let usuarioHtml='<div class="cardt "><a href="#collapseCardT1" class="d-card cardt-header collapsed" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCardT1"><h6 class="m-0 font-weight-bold text-primary">Pedido N°'+pedido.numero+'</h6></a><div class="collapse " id="collapseCardT1"><div class="cardt-body">'+pedido.Lista_Productos+'</div></div></div><tr><td>'
+		
+		listadoHtml+=usuarioHtml;
+	}
+	document.getElementById("Pedidos").innerHTML = listadoHtml;
+    
+  
+}
