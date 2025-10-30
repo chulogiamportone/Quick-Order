7008f6712e2a16715ef937bed9d9d24de8f1bf27
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-10-30 00:07:43 -0300

    Refactor template

--- src/main/resources/static/js/templates.js
+++ src/main/resources/static/js/templates.js
@@ -1,123 +1,151 @@
-
-
-
-$(document).ready(function() {
-	switch ("MOZO") {
-		case "MOZO":
-			cargarPedidosMozo();
-			break;
-		case "CAJA":
-			cargarPedidosCaja();
-			break;
-		case "COCINA":
-			cargarPedidosCocina();
-			break;
-		default:
-			location.href = "login"
-			break;
-	}
+$(document).ready(function () {
+  switch ("MOZO") {
+    case "MOZO":
+      cargarPedidosMozo();
+      break;
+    case "CAJA":
+      cargarPedidosCaja();
+      break;
+    case "COCINA":
+      cargarPedidosCocina();
+      break;
+    default:
+      location.href = "login";
+      break;
+  }
 });
 
-
 async function cargarPedidosMozo() {
+  const response = await fetch("pedidos", {
+    method: "GET",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+  });
 
-	const response = await fetch('pedidos', {
-		method: 'GET',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+  const pedidos = await response.json();
 
-	});
-	const pedidos = await response.json();
-	
-	let listadoHtml = '';
-	let pasada = 0;
-	for (let pedido of pedidos.data) {
-		pasada++
-		let usuarioHtml =								
-		
-		 '<div class="cardt ">' +
-			'<a  onclick="javascript: mostrarPopup('+pedido.id+');" class="d-card cardt-header " role="button">' +
-			'<h4 class="m-0 font-weight-bold text-primary">Pedido N°'+pedido.numero+'</h4></a></div>'
-		listadoHtml += usuarioHtml;
-	}
-	document.getElementById("Pedidos").innerHTML = listadoHtml;
-	document.getElementById('btn-flotante').innerHTML = '<a href="carrito" class="btn-flotante" id="btn-flotante">INICIAR PEDIDO</a>';
-	
+  let listadoHtml = "";
+  let pasada = 0;
 
+  for (let pedido of pedidos.data) {
+    pasada++;
+
+    // Extraer solo la hora y minutos de la cadena fechyHoraDePedido
+    let hora = "";
+    if (pedido.fechyHoraDePedido) {
+      const partes = pedido.fechyHoraDePedido.split(" ");
+      if (partes.length > 1) {
+        // partes[1] es "HH:mm:ss"
+        const horaMin = partes[1].split(":");
+        if (horaMin.length >= 2) {
+          hora = `${horaMin[0]}:${horaMin[1]}`;
+        } else {
+          hora = partes[1];
+        }
+      } else {
+        hora = pedido.fechyHoraDePedido;
+      }
+    }
+
+      // Definir color según estado
+      let estadoColor = "";
+      if (pedido.estado === "En Preparación") {
+        estadoColor = "#EF8808";
+      } else if (pedido.estado === "Entregado") {
+        estadoColor = "#51A360";
+      } else if (pedido.estado === "Cancelado") {
+		estadoColor = "#D63939";
+	  }
+
+      let usuarioHtml = `
+            <div class="cardt ">
+              <a onclick="javascript: mostrarPopup(${pedido.id});" class="d-card cardt-header" role="button">
+                <h4 class="m-0 font-weight-bold text-primary h4-cardt">Pedido N°${pedido.numero}</h4>
+				<p class="m-0 hora-cardt"> Hora: ${hora}</p>
+                <p class="m-0 estado-cardt" style="color:${estadoColor}"> Estado: ${pedido.estado}</p>
+              </a>
+            </div>
+          `;
+
+    listadoHtml += usuarioHtml;
+  }
+  document.getElementById("Pedidos").innerHTML = listadoHtml;
+  document.getElementById("btn-flotante").innerHTML =
+    '<a href="carrito" class="btn-flotante" id="btn-flotante">INICIAR PEDIDO</a>';
 }
 async function cargarPedidosCaja() {
+  const response = await fetch("pedidos", {
+    method: "GET",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+  });
+  const pedidos = await response.json();
 
-	const response = await fetch('pedidos', {
-		method: 'GET',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-
-	});
-	const pedidos = await response.json();
-
-	let listadoHtml = '';
-	let pasada = 0;
-	for (let pedido of pedidos.data) {
-		pasada++
-		let usuarioHtml = '<div class="cardt ">' +
-			'<a  onclick="javascript: mostrarPopup('+pedido.id+');" class="d-card cardt-header " role="button">' +
-			'<h4 class="m-0 font-weight-bold text-primary">Pedido N°'+pedido.numero+'</h4></a></div>'
-		listadoHtml += usuarioHtml;
-	}
-	document.getElementById("Pedidos").innerHTML = listadoHtml;
+  let listadoHtml = "";
+  let pasada = 0;
+  for (let pedido of pedidos.data) {
+    pasada++;
+    let usuarioHtml =
+      '<div class="cardt ">' +
+      '<a  onclick="javascript: mostrarPopup(' +
+      pedido.id +
+      ');" class="d-card cardt-header " role="button">' +
+      '<h4 class="m-0 font-weight-bold text-primary">Pedido N°' +
+      pedido.numero +
+      "</h4></a></div>";
+    listadoHtml += usuarioHtml;
+  }
+  document.getElementById("Pedidos").innerHTML = listadoHtml;
 }
 async function cargarPedidosCocina() {
+  const response = await fetch("pedidos", {
+    method: "GET",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+  });
+  const pedidos = await response.json();
 
-	const response = await fetch('pedidos', {
-		method: 'GET',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-
-	});
-	const pedidos = await response.json();
-
-	let listadoHtml = '';
-	let pasada = 0;
-	for (let pedido of pedidos.data) {
-		pasada++
-		let usuarioHtml = '<div class="cardt ">' +
-			'<a  onclick="javascript: mostrarPopup('+pedido.id+');" class="d-card cardt-header " role="button">' +
-			'<h4 class="m-0 font-weight-bold text-primary">Pedido N°'+pedido.numero+'</h4></a></div>'
-		listadoHtml += usuarioHtml;
-	}
-	document.getElementById("Pedidos").innerHTML = listadoHtml;
-	
+  let listadoHtml = "";
+  let pasada = 0;
+  for (let pedido of pedidos.data) {
+    pasada++;
+    let usuarioHtml =
+      '<div class="cardt ">' +
+      '<a  onclick="javascript: mostrarPopup(' +
+      pedido.id +
+      ');" class="d-card cardt-header " role="button">' +
+      '<h4 class="m-0 font-weight-bold text-primary">Pedido N°' +
+      pedido.numero +
+      "</h4></a></div>";
+    listadoHtml += usuarioHtml;
+  }
+  document.getElementById("Pedidos").innerHTML = listadoHtml;
 }
 
-
 async function mostrarPopup(a) {
-	
-	const response = await fetch('pedidos', {
-		method: 'GET',
-		headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+  const response = await fetch("pedidos", {
+    method: "GET",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+  });
+  const pedidos = await response.json();
+  document.getElementById("popup2").style.display = "block";
 
-	});
-	const pedidos = await response.json();
-	document.getElementById("popup2").style.display = "block";
-	
-	for (let pedido of pedidos.data) {
-		if (pedido.id==a){
-			document.getElementById("popup2").innerHTML='<div class="pop-titulo"><h2 class="pop-pedido">Pedido N°'+pedido.id+'</h2>'+	    	
-			'<h4 class="pop-estado">'+pedido.estado+'</h4></div><h4 class="titulo-prod">Productos</h4><div class="cargar-prod">	'+	
-			'<h4 class="prod">titulo producto</h4><div class="detalle-prod"><h6 class="detalle">detalle</h6>'+   				
-			'<h6 class="precio">precio</h6></div></div><div class="titulo-comentario"><div class="comentario">'+			
-			'</div></div><div class="titulo-prod"><button id="boton-cerrar2" onclick="javascript: cerrarPopup();">Cerrar'+
-			'</button></div>'
-		}
-		
-		
-		
-	
-	}
-	
-	
+  for (let pedido of pedidos.data) {
+    if (pedido.id == a) {
+      document.getElementById("popup2").innerHTML =
+        '<div class="pop-titulo"><h2 class="pop-pedido">Pedido N°' +
+        pedido.id +
+        "</h2>" +
+        '<h4 class="pop-estado">' +
+        pedido.estado +
+        '</h4></div><h4 class="titulo-prod">Productos</h4><div class="cargar-prod">	' +
+        '<h4 class="prod">titulo producto</h4><div class="detalle-prod"><h6 class="detalle">detalle</h6>' +
+        '<h6 class="precio">precio</h6></div></div><div class="titulo-comentario"><div class="comentario">' +
+        '</div></div><div class="titulo-prod"><button id="boton-cerrar2" onclick="javascript: cerrarPopup();">Cerrar' +
+        "</button></div>";
+    }
+  }
 }
 
 function cerrarPopup() {
-	document.getElementById("popup2").style.display = "none";
+  document.getElementById("popup2").style.display = "none";
 }
 document.getElementById("boton-abrir2").addEventListener("click", mostrarPopup);
 document.getElementById("boton-cerrar2").addEventListener("click", cerrarPopup);
-
